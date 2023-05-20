d43b4e37314fa48905605b858211d344993e80b1
Lancelot Tshikota <dvanskot@gmail.com>
2023-05-20 09:35:39 +0200

    StringManager Class And Its TransformWords function added

--- LightstoneAssessment/Program.cs
+++ LightstoneAssessment/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -10,4 +12,46 @@
 internal class Inputs
 {
     public string InputString { get; set; } = string.Empty;
+}
+
+//Class only responsible for transforming strings
+internal class StringManager
+{
+    //Function to reverse string to the desired output
+    public string TransformWords(string input)
+    {
+
+        var transformedString = new StringBuilder();
+        bool whiteSpace = false;
+
+        int x = input.Length - 1;
+        int e = input.Length - 1;
+
+        while (e <= x && e >= 0)
+        {
+            if (input[e] == ' ')
+            {
+                if (x - e > 0)
+                {
+                    if (whiteSpace)
+                    {
+                        transformedString.Append(" ");
+                    }
+                    transformedString.Append(input.Substring(e + 1, x - e));
+                    whiteSpace = true;
+                }
+                x = e - 1;
+            }
+            e--;
+        }
+        if (x - e > 0)
+        {
+            if (whiteSpace)
+            {
+                transformedString.Append(" ");
+            }
+            transformedString.Append(input.Substring(e + 1, x - e));
+        }
+        return transformedString.ToString();
+    }
 }
